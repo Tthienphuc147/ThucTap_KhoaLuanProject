f5b810c5344eed6656e36c797e37901b38212025
Tthienphuc147 <43287192+Tthienphuc147@users.noreply.github.com>
2020-05-20 08:04:18 +0700

    --update

--- khoaluan/angular/Project_angular/src/app/share/share.module.ts
+++ khoaluan/angular/Project_angular/src/app/share/share.module.ts
@@ -7,7 +7,7 @@ import { TimeAgoPipe } from 'time-ago-pipe';
 import { DemoMaterialModule } from '../admin/masterial-module';
 import { NoSanitizePipe } from '../pipe/no-sanitize.pipe';
 import { TextTruncatePipe } from '../pipe/text-truncate.pipe';
-import { NgxImageZoomModule } from 'ngx-image-zoom';
+
 @NgModule({
     declarations: [TextTruncatePipe, TimeAgoPipe, NoSanitizePipe],
     imports: [
@@ -17,7 +17,6 @@ import { NgxImageZoomModule } from 'ngx-image-zoom';
         FormsModule,
         NgbModule,
         RouterModule,
-        NgxImageZoomModule
     ],
     exports: [
         DemoMaterialModule,
@@ -29,7 +28,6 @@ import { NgxImageZoomModule } from 'ngx-image-zoom';
         NoSanitizePipe,
         RouterModule,
         CommonModule,
-        NgxImageZoomModule
     ]
 })
 export class ShareModule {}
